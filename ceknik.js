879c452c407cb642bf5536d1db27f41529941b39
Jabal Surya Ngalam <87789231+Jabalsurya2105@users.noreply.github.com>
2024-07-02 10:34:52 +0700

    Add files via upload

--- ceknik.js
+++ ceknik.js
@@ -19,7 +19,7 @@ function T(e, t) {
 let n; const r = typeof e; return "boolean" === r ? +e : "string" === r ? (n = parseInt(e, t || 10), isNaN(n) || !isFinite(n) ? 0 : n) : "number" === r && isFinite(e) ? 0 | e : 0
 }
 
-export function cekNIK(nik) {
+export default function cekNIK(nik) {
 // Data wilayah & kodepos
 const U = {
 "provinsi": { 
